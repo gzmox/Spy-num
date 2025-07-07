956d1d01148dcf690a692747e2b435db1c59a358
gzmox <mush.tbk@gmail.com>
2025-07-07 18:37:07 +0200

    Delete search.js

--- search.js
+++
@@ -1,4 +0,0 @@
-export function createSearchUrl(name) {
-  const query = encodeURIComponent(\`\${name} site:linkedin.com OR site:facebook.com\`);
-  return \`https://www.google.com/search?q=\${query}\`;
-}
